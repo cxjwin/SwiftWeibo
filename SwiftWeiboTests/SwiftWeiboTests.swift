b68971b02bf8a30eeea256a5d759a7c1a7afaa58
SmartCai <88cxjwin@gmail.com>
2014-10-26 18:31:49 +0800

    Initial Commit

---
+++ SwiftWeiboTests/SwiftWeiboTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftWeiboTests.swift
+//  SwiftWeiboTests
+//
+//  Created by cxjwin on 10/26/14.
+//  Copyright (c) 2014 cxjwin. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SwiftWeiboTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
